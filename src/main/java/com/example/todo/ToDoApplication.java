d6b006b06321300cb95cd132a953d850619c25fc
“Roman <fedkoroma2@gmail.comgit config --global user.email fedkoroma2@gmail.com>
2023-09-25 13:17:36 +0300

    Just started(1)

--- src/main/java/com/example/todo/ToDoApplication.java
+++ src/main/java/com/example/todo/ToDoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ToDoApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ToDoApplication.class, args);
     }
 
